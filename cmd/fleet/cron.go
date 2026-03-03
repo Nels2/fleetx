f02fd83841229b5138979c9caa484c5280ebeaf9
Nelson Orellana <nnels227@gmail.com>
2026-03-03 11:05:24 -0700

    Edits

--- cmd/fleet/cron.go
+++ cmd/fleet/cron.go
@@ -49,7 +49,6 @@ import (
 	"github.com/fleetdm/fleet/v4/server/vulnerabilities/winoffice"
 	"github.com/fleetdm/fleet/v4/server/webhooks"
 	"github.com/fleetdm/fleet/v4/server/worker"
-	kitlog "github.com/go-kit/log"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -363,7 +362,7 @@ func scanVulnerabilities(
 			if err := worker.QueueFreeScoutVulnJobs(
 				automationCtx,
 				ds,
-				kitlog.With(logger, "freescout", "vulnerabilities"),
+				logger.With("freescout", "vulnerabilities"),
 				recentV,
 				matchingMeta,
 			); err != nil {
